4a3b8fa319bff7ed8f7d20559adde4a462632125
Luiz Moraes <lffm1994@gmail.com>
2021-07-05 02:19:54 -0300

    Refactoring

--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,17 +8,9 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
 	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-		if (desempenho == Desempenho.A_DESEJAR) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-			funcionario.reajusteSalario(reajuste);
-		} else if (desempenho == Desempenho.BOM) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-			funcionario.reajusteSalario(reajuste);
-		} else {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-			funcionario.reajusteSalario(reajuste);
-		}
-
+		BigDecimal percentual = desempenho.percentualReajuste();
+		BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+		funcionario.reajusteSalario(reajuste);
 	}
 
 }
